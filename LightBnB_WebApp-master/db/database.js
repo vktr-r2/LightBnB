49d189df4816b5ca4f55d566096734d429dca6a7
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-04 14:08:45 -0400

    Add pg package to project.  Add functionality for site to pull properties from db

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,13 +1,24 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+
+
 /// Users
 
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
- */
+*/
 const getUserWithEmail = function (email) {
   let resolvedUser = null;
   for (const userId in users) {
@@ -23,7 +34,7 @@ const getUserWithEmail = function (email) {
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
- */
+*/
 const getUserWithId = function (id) {
   return Promise.resolve(users[id]);
 };
@@ -32,7 +43,7 @@ const getUserWithId = function (id) {
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
- */
+*/
 const addUser = function (user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
@@ -46,26 +57,27 @@ const addUser = function (user) {
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
- */
+*/
 const getAllReservations = function (guest_id, limit = 10) {
   return getAllProperties(null, 2);
 };
 
 /// Properties
 
-/**
- * Get all properties.
- * @param {{}} options An object containing query options.
- * @param {*} limit The number of results to return.
- * @return {Promise<[{}]>}  A promise to the properties.
- */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      return(result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
+
 
 /**
  * Add a property to the database
