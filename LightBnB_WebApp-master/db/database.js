e5ae3d3bb9de02a8b12c236a0b1b1b7615f642b1
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-04 22:19:59 -0400

    Add getUserById functionality

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -33,7 +33,14 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
 */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      return(result.rows[0]);
+    })
+    .catch((err) => {
+      return null;
+    });
 };
 
 /**
